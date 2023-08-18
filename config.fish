eedbce8d3c636eb25c85817bf9028e162d2df8e9
Aaron Blum <blumaa@gmail.com>
2023-08-18 11:34:57 +0200

    updated config.fish

--- config.fish
+++ config.fish
@@ -1,20 +1,21 @@
-alias bs='bin/setup'
-alias bd='bin/dev'
+alias vim='nvim'
+alias vimconfig='vim ~/.config/nvim/init.lua'
+alias zconfig='vim ~/.zshrc'
+alias kt='cd /Users/flatironschool/Development/kinkytowers1/'
+alias nn='cd /Users/flatironschool/Development/net-ninja/'
+alias stage='cd /Users/flatironschool/Development/stage/'
+alias development='cd /Users/flatironschool/Development/'
+alias fishconfig='vim ~/.config/fish/config.fish'
+
+# aliases
 alias br='git branch'
 alias ci='git commit -m'
 alias gst='git status'
 alias gd='git diff'
 alias co='git checkout'
-alias co='git checkout'
 alias cob='git checkout -b'
-alias grom='git rebase origin/main'
-
-alias lococheck='yarn prettier-check -w && yarn tsc && yarn lint'
-
-# alias ls='ls -lsh --color=auto | lolcat'
 alias ls='ls -lsh --color=auto'
-# alias bi='bundle install'
-# alias ni='npm install'
+alias grom='git rebase origin/main'
 
 # tmux aliases
 # alias xnew='tmux new-session -t'
@@ -28,71 +29,56 @@ abbr -a xls 'tmux list-sessions'
 # abbr -a tksv 'tmux kill-server'
 abbr -a xk 'tmux kill-session -t'
 abbr -a xrr 'tmux resize-pane -R 10'
-abbr -a xrl 'tmux resize-pane -L 10'
 
-
-#localyze alias
-# alias bes='bundle exec rails s'
-# alias beside='bundle exec sidekiq'
-# alias fes='yarn start'
-
-  # Set Spaceship ZSH as a prompt
-# autoload -U promptinit; promptinit
-# prompt spaceship
-
-function gbso 
-  git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)' | head -n 25
+# --------------------------------------------------------
+# this is a custom function to switch branches easily 
+# --------------------------------------------------------
+function gbso
+    git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)' | head -n 25
 end
 
-function gon 
-  gbso | head -n 15 | nl
-  echo 'Which branch: '
-  read branch_number
-  set branch_name $(gbso | awk "NR==$branch_number" | tr -d ' ')
-  git checkout $branch_name
-  # update_current_git_vars # remove if not using zsh-git-prompt
+function gon
+    gbso | head -n 15 | nl
+    echo 'Which branch: '
+    read branch_number
+    set branch_name $(gbso | awk "NR==$branch_number" | tr -d ' ')
+    git checkout $branch_name
+    # update_current_git_vars # remove if not using zsh-git-prompt
 end
 
-# if type rg &> /dev/null; then
-#   export FZF_DEFAULT_COMMAND='rg --files'
-#   export FZF_DEFAULT_OPTS='-m --height 50% --border'
-# fi
-
-alias vim='nvim'
-alias vimconfig='vim ~/.config/nvim/init.lua'
-alias kittyconfig='vim ~/.config/kitty/kitty.conf'
-alias zconfig='vim ~/.zshrc'
-alias fishconfig='vim ~/.config/fish/config.fish'
-
-alias dev='cd ~/Code/'
-alias loc='cd ~/Code/localyze/Backend/'
-alias pluto='cd ~/Code/localyze/pluto/'
-alias talent='cd ~/Code/localyze/talent-web/'
-alias stage='cd ~/Code/stage/'
-
-# if status is-interactive
-#     # Commands to run in interactive sessions can go here
-# end
-
-function nvim-astro
-    env NVIM_APPNAME=nvim-astro nvim
+if type rg &>/dev/null
+    export FZF_DEFAULT_COMMAND='rg --files'
+    export FZF_DEFAULT_OPTS='-m --height 50% --border'
 end
 
-function default
-    env NVIM_APPNAME= nvim
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
 
-function nvim-chad
-    env NVIM_APPNAME=nvim-chad nvim
+# --------------------------------------------------------
+# these are options for custom nvim environments
+# --------------------------------------------------------
+function AstroNvim
+    env NVIM_APPNAME=AstroNvim nvim
+end
+
+function default 
+    env NVIM_APPNAME=nvim
+end
+
+function LazyNvim 
+    env NVIM_APPNAME=LazyNvim nvim
 end
 
 function nvims
-    set items default nvim-astro nvim-chad
-    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    set items default AstroNvim LazyNvim 
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config = " --height=~50% --layout=reverse --border --exit-0)
+    echo $config
     if [ -z $config ]
         echo "Nothing selected"
         return 0
-    else if [ $config = "default" ]
+    else if [ $config = default ]
         set config ""
     end
     env NVIM_APPNAME=$config nvim $argv
@@ -100,4 +86,4 @@ end
 
 bind \ca nvims
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+source /usr/local/opt/asdf/libexec/asdf.fish
